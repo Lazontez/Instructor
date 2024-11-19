48a192444aa80a259269b52e080e187e2083129d
Lazontez <tezgardner1212@gmail.com>
2024-11-18 22:43:57 -0600

    Added Functionality to Edit a task

--- instructor/src/App.jsx
+++ instructor/src/App.jsx
@@ -9,16 +9,27 @@ function App() {
     setTasks([...tasks, newTask]); // Add the new task to the existing array
   };
 
-   // Remove a task from the list
-   const removeTask = (taskId) => {
+  // Remove a task from the list
+  const removeTask = (taskId) => {
     setTasks(tasks.filter((task) => task.id !== taskId));
   };
+
+  function editTask(taskId, newTaskText) {
+    setTasks(tasks.map((task) => {
+      // If task.id matches taskId, update the task
+      if (task.id === taskId) {
+        return { ...task, name: newTaskText }; // Return updated        
+      }
+      // If task.id doesn't match taskId, return the task as is
+      return task;
+    }));
+  }
 
   return (
     <div>
       <h1>Guitar Lesson Tracker</h1>
       <LogForm addTask={addTask} />
-      <TaskList tasks={tasks} removeTask={removeTask} />
+      <TaskList tasks={tasks} removeTask={removeTask} editTask={editTask} />
     </div>
   );
 }
--- instructor/src/components/TaskLists.jsx
+++ instructor/src/components/TaskLists.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const TaskList = ({ tasks , removeTask }) => {
+const TaskList = ({ tasks , removeTask, editTask }) => {
 
-    function SaveTask (){
-        console.log(tasks)
+  function getNewText(){
+    const newText = window.prompt('What would you like to update the task name to?')
+    if (newText){
+      return newText
     }
+    else{
+      return 'No Task Name Entered. Press Edit To Update '
+    }
+  }
 
   return (
    <ul>
       {tasks.map((task) => (
         <li key={task.id}>{task.name}  
-        <button onClick={() => removeTask(task.id)}>Delete</button> {/* This button removes the task */}      
+        <button onClick={() => removeTask(task.id)}>Delete</button>
+        <button onClick={() => editTask(task.id, getNewText())}>Edit</button>{/* This button removes the task */} 
+
         </li>      
       ))}
     </ul>    
