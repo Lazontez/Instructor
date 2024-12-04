f950ef67c45a77b6acc40770d4889128c0433cf4
Lazontez <tezgardner1212@gmail.com>
2024-12-03 18:48:50 -0600

    tesst

--- instructor/src/App.jsx
+++ instructor/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login.jsx'; // Import login component
 import SignUp from './components/SignUp.jsx'
 import TeacherDashboard from './components/TeacherDashboard'; // Import teacher dashboard
