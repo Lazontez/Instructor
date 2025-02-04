f19d6556c661bfc6d7c69b3ae76f82b26bad6ff7
Lazontez <tezgardner1212@gmail.com>
2025-02-04 12:19:13 -0600

    changing rippple

--- instructor/src/components/Header.jsx
+++ instructor/src/components/Header.jsx
@@ -5,49 +5,54 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import logo from '../assets/logos/pro-guitar-instructor-logo.png'
-import '../utils/Header.css'
-
+import logo from '../assets/logos/pro-guitar-instructor-logo.png';
+import '../utils/Header.css';
 
 const Header = () => {
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const handleLogOut = () => {
-        localStorage.removeItem('token')
-        localStorage.removeItem('role')
-        navigate('/login')
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        navigate('/login');
+    };
 
-    }
     return (
         <AppBar
             position="sticky"
             sx={{
                 backgroundColor: '#f4f4f9',
                 boxShadow: 'none',
-                color: 'black',  
+                color: 'black',
             }}
         >
-            {/* Container creates horizontal space like modern navbars */}
-            <Container maxWidth="lg">
-                {/* disableGutters removes Toolbar's default left/right padding */}
-                <Toolbar disableGutters sx={{ minHeight: '50px' }}>
-                    <Box sx={{ flexGrow: 1 }}>
+            <Container maxWidth="lg" disableGutters>
+                <Toolbar
+                    disableGutters
+                    sx={{
+                        minHeight: '50px',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box >
                         <img
                             src={logo}
                             alt="Guitar Instructor Pro Logo"
-                            style={{ height: '100px', marginRight: '16px' }}
+                            style={{ height: '60px', marginRight: '16px' }}
                         />
                     </Box>
-                    <Button
+                    <Button 
+                    disableRipple
+                    disableFocusRipple
                         color="inherit"
                         sx={{
                             textTransform: 'none',
                             padding: '6px 12px',
-                            
                         }}
-                        onClick={() => handleLogOut()}
-                        id='logOutBtn'
+                        onClick={handleLogOut} 
+                        id="logOutBtn"
                     >
                         Logout
                     </Button>
@@ -58,6 +63,7 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
 
